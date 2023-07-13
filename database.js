daa88387ab1d7ba0207f8b57e47e34e17556934d
Papil Kamdi <papilkamdi786@gmail.com>
2023-07-13 19:37:09 +0530

    Changes

--- database.js
+++ database.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 
-const MONGO_URI = "mongodb+srv://tempData:tempdata12345@cluster0.zgpdcsv.mongodb.net/Seat_Reservation?retryWrites=true&w=majority";
+const MONGO_URI = "mongodb+srv://papil1997:Papil123@cluster0.mgod2.mongodb.net/Seat_Reservation?retryWrites=true&w=majority";
 
-// "mongodb+srv://papil1997:Papil123*@cluster0.mgod2.mongodb.net/Seat_Reservation?retryWrites=true&w=majority";
+// "mongodb+srv://tempData:tempdata12345@cluster0.zgpdcsv.mongodb.net/Seat_Reservation?retryWrites=true&w=majority";
+
+// ;
 
 mongoose
   .connect(MONGO_URI, {
